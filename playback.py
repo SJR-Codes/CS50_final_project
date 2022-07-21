54c620e5d9d1114f16c94e468ca6539f46282303
SJR-Codes <SJR-Codes@users.noreply.github.com>
2022-07-21 11:50:49 +0000

    automated commit by check50 [check50=True]

---
+++ playback.py
@@ -0,0 +1,11 @@
+"""
+* CS50P Problem Set 0
+* Playback Speed
+* by Samu Reinikainen
+"""
+
+#get users input
+userinput = input("Enter your input, please: ")
+
+#print user input spaces replaced with "..."
+print(userinput.replace(" ", "..."))
