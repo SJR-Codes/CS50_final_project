c0805caf7757a6ad08d748985daf3d6625d9671c
SJR-Codes <SJR-Codes@users.noreply.github.com>
2022-07-22 07:46:17 +0000

    automated commit by check50 [check50=True]

--- extensions.py
+++ extensions.py
@@ -7,15 +7,19 @@
 
 fname = input("Enter file name: ").strip().lower()
 img_exts = ["gif", "jpg", "jpeg", "png"]
-app_exts = ["pdf", "txt", "zip"]
+app_exts = ["pdf", "zip"]
+app_txts = ["txt"]
 
 
 if "." in fname:
     ext = fname.split(".")
-    if ext[1] in img_exts:
+
+    if ext[-1] in img_exts:
         print("image/" + ext[1])
-    elif ext[1] in app_exts:
+    elif ext[-1] in app_exts:
         print("application/" + ext[1])
+    elif ext[-1] in app_txts:
+        print("text/plain")
     else:
         print("application/octet-stream")
 else:
