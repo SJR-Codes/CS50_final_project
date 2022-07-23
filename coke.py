dbac053f3dd0695f9109d35213265f1287f21843
SJR-Codes <SJR-Codes@users.noreply.github.com>
2022-07-23 17:49:04 +0000

    automated commit by check50 [check50=True]

--- coke.py
+++ coke.py
@@ -9,11 +9,10 @@ accept_change = [25, 10, 5]
 amount = 0
 
 while amount < coke_price:
-    #change = int(input("Enter money(25c, 10c or 5c): "))
-    change = int(input())
+    change = int(input("Enter money: "))
     if change in accept_change:
         amount += change
     if coke_price - amount > 0:
-        print("Amount due:", str(coke_price - amount), " ")
+        print("Amount due:", str(coke_price - amount), " ", end="")
 
 print("Change owed:", str(amount - coke_price), " ")
