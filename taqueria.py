afa12070cf4e723122fc2040c779f54a69fbb1f5
SJR-Codes <SJR-Codes@users.noreply.github.com>
2022-07-24 12:51:10 +0000

    automated commit by check50 [check50=True]

---
+++ taqueria.py
@@ -0,0 +1,35 @@
+"""
+* CS50P Problem Set 3
+* Felipe’s Taqueria
+* by Samu Reinikainen 24.07.2022
+"""
+
+def main():
+    price = (get_input("What's your pleasure, sir? "))
+
+    print(f"Total: ${price:.2f}")
+
+def get_input(prompt):
+    price = 0
+    while True:
+        try:
+            price += get_price(input(prompt))
+        except EOFError:
+            return price
+
+def get_price(item):
+    menu = {
+        "Baja Taco": 4.00,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00
+    }
+
+    return menu.get(item.lower().title(), 0)
+
+main()
