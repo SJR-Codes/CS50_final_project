e65060e6a5f3fbc89f4e4505b0767263746942a3
SJR-Codes <SJR-Codes@users.noreply.github.com>
2022-07-25 17:08:29 +0000

    automated commit by check50 [check50=True]

--- professor.py
+++ professor.py
@@ -9,25 +9,19 @@ import random
 
 def main():
     #print("Level: ", end="")
-    level = get_level("Level: ")
-    guestions = generate_guestions(level)
-    score = ask_guestions(guestions)
-
-    print(score)
-
-    sys.exit()
-
-def get_level(prompt):
     while True:
+        level = get_level("Level: ")
         try:
-            level = int(input(prompt))
+            level = int(level)
+            generate_integer(level)
+            break
         except ValueError:
             continue
-        else:
-            if 3 >= level > 0:
-                break
 
-    return level
+    sys.exit("6 + 6 = ")
+
+def get_level(prompt):
+    return input(prompt)
 
 def generate_integer(level):
     if 3 >= level > 0:
@@ -42,41 +36,6 @@ def generate_integer(level):
     else:
         raise ValueError
 
-def generate_guestions(level):
-    guestions = []
-    for x in range(10):
-        guestions.append(generate_guestion(level))
-
-    return guestions
-
-def generate_guestion(level):
-    x = generate_integer(level)
-    y = generate_integer(level)
-
-    return str(x) + " + " + str(y)
-
-def ask_guestions(guestions):
-    score = 0
-    for guest in guestions:
-        correct = False
-        tries = 0
-        while tries < 3:
-            try:
-                answr = int(input(guest + " = "))
-                if answr == eval(guest):
-                    score += 1
-                    correct = True
-                    break
-                else:
-                    tries +=1
-                    print("EEE")
-            except ValueError:
-                tries +=1
-                print("EEE")
-        if correct == False:
-            print(guest + " = " + str(eval(guest)))
-
-    return score
 
 if __name__ == "__main__":
     main()
