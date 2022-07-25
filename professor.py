4a24d40c9ef19c9851e13ae626f930ebcbf222ca
SJR-Codes <SJR-Codes@users.noreply.github.com>
2022-07-25 17:57:02 +0000

    automated commit by check50 [check50=True]

--- professor.py
+++ professor.py
@@ -8,11 +8,14 @@ import sys
 import random
 
 def main():
+    #print("Level: ", end="")
     level = get_level()
+    guestions = generate_guestions(level)
+    score = ask_guestions(guestions)
 
-    rint = generate_integer(level)
+    print(score)
 
-    print(rint)
+    sys.exit()
 
 def get_level():
     while True:
@@ -22,7 +25,9 @@ def get_level():
             continue
         else:
             if 3 >= level > 0:
-                return level
+                break
+
+    return level
 
 def generate_integer(level):
     if 3 >= level > 0:
@@ -37,6 +42,41 @@ def generate_integer(level):
     else:
         raise ValueError
 
+def generate_guestions(level):
+    guestions = []
+    for x in range(10):
+        guestions.append(generate_guestion(level))
+
+    return guestions
+
+def generate_guestion(level):
+    x = generate_integer(level)
+    y = generate_integer(level)
+
+    return str(x) + " + " + str(y)
+
+def ask_guestions(guestions):
+    score = 0
+    for guest in guestions:
+        correct = False
+        tries = 0
+        while tries < 3:
+            try:
+                answr = int(input(guest + " = "))
+                if answr == eval(guest):
+                    score += 1
+                    correct = True
+                    break
+                else:
+                    tries +=1
+                    print("EEE")
+            except ValueError:
+                tries +=1
+                print("EEE")
+        if correct == False:
+            print(guest + " = " + str(eval(guest)))
+
+    return score
 
 if __name__ == "__main__":
     main()
