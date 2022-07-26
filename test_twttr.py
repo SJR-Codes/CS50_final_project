291bbe1932f1458b2046daeaef43b9ea65d9c76f
SJR-Codes <SJR-Codes@users.noreply.github.com>
2022-07-26 08:58:24 +0000

    automated commit by check50 [check50=True]

--- test_twttr.py
+++ test_twttr.py
@@ -1,5 +1,4 @@
 from twttr import shorten
-import pytest
 
 def test_words():
     assert shorten("Twitter") == "Twttr"
@@ -21,7 +20,7 @@ def test_numeric():
     assert shorten(666) == "666"
 
 def test_empty():
-    assert shorten() == ""
+    assert shorten("") == ""
 
 def test_chars():
     assert shorten(".") == "."
